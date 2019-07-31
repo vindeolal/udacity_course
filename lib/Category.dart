55571b71f9e6d181e415e52f0690259003e380ec
Vinod Deolal <vinoddeolal@gmail.com>
2019-07-31 21:50:46 +0530

    add navigation to mocked converter screen

--- lib/Category.dart
+++ lib/Category.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
 
+import 'ConverterRoute.dart';
+import 'Unit.dart';
+
 class Category extends StatelessWidget {
   final IconData icon;
   final Color color;
   final String name;
+  final List<Unit> units;
 
-  const Category({
-    Key key,
-    @required this.name,
-    @required this.color,
-    @required this.icon,
-  })  : assert(name != null),
+  const Category(
+      {Key key,
+      @required this.name,
+      @required this.color,
+      @required this.icon,
+      @required this.units})
+      : assert(name != null),
         assert(color != null),
         assert(icon != null),
+        assert(units != null),
         super(key: key);
+
+  void _navigateToConverter(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: Text(
+            name,
+            style: Theme.of(context).textTheme.display1,
+          ),
+          centerTitle: true,
+          backgroundColor: color,
+        ),
+        body: ConverterRoute(
+          color: color,
+          units: units,
+        ),
+      );
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +52,7 @@ class Category extends StatelessWidget {
           borderRadius: BorderRadius.circular(5.0),
           highlightColor: color,
           splashColor: color,
-          onTap: () => print("tapped"),
+          onTap: () => _navigateToConverter(context),
           child: Padding(
             padding: EdgeInsets.all(8.0),
             child: Row(
--- lib/CategoryRoute.dart
+++ lib/CategoryRoute.dart
@@ -1,52 +1,53 @@
 import 'package:flutter/material.dart';
 
 import 'Category.dart';
+import 'Unit.dart';
 
 class CategoryRoute extends StatelessWidget {
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
+
+  List<Unit> _retrieveUnitList(String categoryName) {
+    return List.generate(10, (int i) {
+      i += 1;
+      return Unit(
+        name: '$categoryName Unit $i',
+        conversion: i.toDouble(),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<Category> categories = [
-      Category(
+    final categories = <Category>[];
+
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColors[i],
         icon: Icons.cake,
-        name: 'Length',
-        color: Colors.teal,
-      ),
-      Category(
-        icon: Icons.cake,
-        name: 'Area',
-        color: Colors.orange,
-      ),
-      Category(
-        icon: Icons.cake,
-        name: 'Volume',
-        color: Colors.pinkAccent,
-      ),
-      Category(
-        icon: Icons.cake,
-        name: 'Mass',
-        color: Colors.blueAccent,
-      ),
-      Category(
-        icon: Icons.cake,
-        name: 'Time',
-        color: Colors.yellow,
-      ),
-      Category(
-        icon: Icons.cake,
-        name: 'Digital Storage',
-        color: Colors.greenAccent,
-      ),
-      Category(
-        icon: Icons.cake,
-        name: 'Energy',
-        color: Colors.purpleAccent,
-      ),
-      Category(
-        icon: Icons.cake,
-        name: 'Currency',
-        color: Colors.red,
-      ),
-    ];
+        units: _retrieveUnitList(_categoryNames[i]),
+      ));
+    }
 
     return Scaffold(
         appBar: AppBar(
